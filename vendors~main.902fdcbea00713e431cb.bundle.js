74c876020c722d6c5799c86607685636e33b233f
Marco Vettorello <vettorello.marco@gmail.com>
2020-02-26 09:45:12 +0100

    Updating docs page

--- vendors~main.902fdcbea00713e431cb.bundle.js
+++ vendors~main.902fdcbea00713e431cb.bundle.js
@@ -151,4 +151,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.55c520ff064601197dde.bundle.js.map
+//# sourceMappingURL=vendors~main.902fdcbea00713e431cb.bundle.js.map
